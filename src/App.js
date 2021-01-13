fb96149bbc16122b1dde42b90ebe0115a610a045
Rebecca Manzi <rebeccamanzi@gmail.com>
2021-01-13 17:22:26 -0300

    reatividade

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React from 'react';
 
 const App = () => {
-  // 'ativo' iniciará como true
-  // setAtivo altera seu valor
-  // setNome é o padrão utilizado 
-  const [ativo, setAtivo] = React.useState(true);
+  const [items, setItems] = React.useState(['Item 1', 'Item 2']);
+
+  function handleClick() {
+    // Errado. Modifique o estado apenas com a função de atualização (setItems)
+    items.push('Novo Item');
+  }
+
+  function handleClickReativo() {
+    // Correto. Eu desestruturo a array atual, criando uma nova e adiciono um novo elemento
+    setItems([...items, 'Novo Item']);
+  }
 
   return (
-    // ao clicar, o valor será atualizado para o seu contrário (!ativo)
-    // isso dará um efeito de ativar e desativar
-    <button onClick={() => setAtivo(!ativo)}>
-      {ativo ? 'Botão Ativo' : 'Botão Inativo'}
-    </button>
+    <>
+      {items.map((item, i) => (
+        <li key={i}>{item}</li>
+      ))}
+      <button onClick={handleClick}>Adicionar Item</button>
+      <button onClick={handleClickReativo}>Adicionar Reativo</button>
+    </>
   );
 };
+
 
 export default App;
---
+++ src/Modal.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Modal = () => {
+  return (
+    <div>
+      Esse é um modal. <button> onClick={() => setModal(false)}Fechar</button>
+    </div>
+  )
+}
+
+export default Modal
